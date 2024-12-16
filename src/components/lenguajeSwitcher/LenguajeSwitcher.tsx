8898bac41a92c8c695d5421c9a1b41dfb642d012
Lau Sierra <laura.sierraJ03@gmail.com>
2024-12-16 13:15:18 +0100

    lenguajeswitcher switched

--- src/components/lenguajeSwitcher/LenguajeSwitcher.tsx
+++ src/components/lenguajeSwitcher/LenguajeSwitcher.tsx
@@ -44,19 +44,19 @@ const LanguageSwitcher: React.FC = () => {
                         className="cursor-pointer hover:bg-red-500 hover:text-white px-2 py-1 rounded"
                         onClick={() => handleLanguageChange("es")}
                     >
-                        Español
+                        {i18n.t('language.espanol')}
                     </li>
                     <li
                         className="cursor-pointer hover:bg-red-500 hover:text-white px-2 py-1 rounded"
                         onClick={() => handleLanguageChange("ca")}
                     >
-                        Català
+                        {i18n.t('language.catalan')}
                     </li>
                     <li
                         className="cursor-pointer hover:bg-red-500 hover:text-white px-2 py-1 rounded"
                         onClick={() => handleLanguageChange("en")}
                     >
-                        English
+                        {i18n.t('language.english')}
                     </li>
                 </ul>
             </div>
