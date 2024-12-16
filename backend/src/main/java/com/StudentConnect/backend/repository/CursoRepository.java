f7fc26f1d55295a8411e0edbb6d07879aa3a0507
Lau Sierra <laura.sierraJ03@gmail.com>
2024-12-16 12:25:13 +0100

    borrar antiguo backend

--- backend/src/main/java/com/StudentConnect/backend/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.StudentConnect.backend.repository;
-
-import com.StudentConnect.backend.entity.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
-}
